8a0aa0b25ae82a790708a066d70234e11c70fa75
Dhruv <97015100+FunCoder15@users.noreply.github.com>
2022-06-24 22:59:39 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+var sideNav = document.getElementById("sideNav");
+function OpenSideNav() {
+    sideNav.style.width = '70vw';
+}
+function CloseSideNav() {
+    sideNav.style.width = '0';
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
